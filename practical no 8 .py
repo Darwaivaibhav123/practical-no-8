9252e8d0aefd18ae081f2775d6a732545534a46b
Darwaivaibhav123 <91363956+Darwaivaibhav123@users.noreply.github.com>
2021-11-18 12:24:10 +0530

    Update practical no 8 .py

--- practical no 8 .py
+++ practical no 8 .py
@@ -14,3 +14,42 @@ def bubble_sort(a) :
             a[j+1] = temp
 bubble_sort(a)
 print("sorted list =",a)
+#insertion sort
+l = int(input("enter nos in list: "))
+a=[]
+for i in range(l):
+  a.append(int(input("")))
+
+def insertion(a):
+  for i in range(len(a)):
+    temp = a[i]
+    j = i-1
+    while j>=0 and a[j]>temp:
+      a [j+1] =a[j]
+      j-=1
+      a[j+1] = temp
+
+insertion(a)
+print(a)
+
+#selection sort
+l = int(input("enter nos in list: "))
+a=[]
+for i in range(l):
+  a.append(int(input("")))
+
+def selectionSort(a, size):
+   
+    for i in range(size):
+        min_idx = i
+
+        for i in range(i + 1, size):
+
+            if a[i] < a[min_idx]:
+                min_idx = i
+         
+        (a[i], a[min_idx]) = (a[min_idx], a[i])
+
+size = len(a)
+selectionSort(a, size)
+print(a)
